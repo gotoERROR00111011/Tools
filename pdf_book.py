a26c54d32e316929d74eca51b3adfbd2dd2026b7
unknown <gotoerror00111011@gmail.com>
2021-05-12 22:17:32 +0900

    _

--- pdf_book.py
+++ pdf_book.py
@@ -22,17 +22,18 @@ def get_ratio(img):
     h = 1
     return h, w
 
-def remove_background(img):
+def remove_background(img, thresh=220):
     if len(img.shape)==2:
-        _, threshold = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
+        _, threshold = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)
     else:
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        _, threshold = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+        _, threshold = cv2.threshold(gray, thresh, 255, cv2.THRESH_BINARY)
         threshold = cv2.cvtColor(threshold, cv2.COLOR_GRAY2BGR)
     img = cv2.add(img, threshold)
     return img
 
 def histogram_compress(img):
+    #img = np.array(img/5, dtype=np.int) * 5
     img = np.array(img/25, dtype=np.int) * 25
     img[img > 235] = 255
     img = np.array(img, dtype=np.float32)
@@ -54,7 +55,7 @@ def imwrite(trg_path, img):
 def convert_page(src_path, mode):
     ff = np.fromfile(src_path, np.uint8)
     img = cv2.imdecode(ff, mode)
-    img = remove_background(img)
+    #img = remove_background(img)
     return resize(img)
 
 def get_all_paths(src_path, trg_path, extension):
@@ -64,11 +65,18 @@ def get_all_paths(src_path, trg_path, extension):
         for f in get_files(d, extension):
             files.append(f)
     return files
+
+def remove_finger(img):
+    h, w = (img.shape[0], img.shape[1])
+    top, bottom = (0, h)
+    left, right = (0, w)
+    img = img[:, left + 30 : right - 30]
+    return img
     
 
 if __name__ == "__main__":
     mode = ["0_original", "1_pdf_to_images", "2_convert_gray", "3_convert_color", "4_images_to_pdf"]
-    select = 1
+    select = 4
 
     if mode[select] == "1_pdf_to_images":
         src_path = mode[0]
@@ -91,12 +99,14 @@ if __name__ == "__main__":
             trg = image.replace(src_path, trg_path)
             img = convert_page(image, cv2.IMREAD_GRAYSCALE)
             img = histogram_compress(img)
+            #img = remove_finger(img)
             imwrite(trg, img)
 
     
     if mode[select] == "3_convert_color":
         src_path = mode[1]
-        trg_path = mode[3]
+        #trg_path = mode[3]
+        trg_path = mode[2]
         images = get_all_paths(src_path, trg_path, "*.png")
         
         for image in images:
